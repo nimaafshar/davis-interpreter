c65fcf2f8cd0cac62f1e4097746b3c26e5af6ded
Nima Afshar <nima.afsharh@gmail.com>
2024-02-06 22:33:07 +0330

    change structure slightly

--- pkg/davis/program.go
+++ pkg/davis/program.go
@@ -9,10 +9,15 @@ import (
 func NewProgram(instructions []Instruction, inputs []int) (Program, error) {
 	labels := make(map[Label]int)
 
-	maxVar := 0
+	maxLocalVarIndex := -1
+	maxInputVarIndex := -1
 	for index, instruction := range instructions {
-		if int(instruction.Var) > maxVar {
-			maxVar = int(instruction.Var)
+		vType, varIdx := instruction.Var.TypeAndIndex()
+		switch vType {
+		case VariableTypeInput:
+			maxInputVarIndex = max(maxInputVarIndex, varIdx)
+		case VariableTypeLocal:
+			maxLocalVarIndex = max(maxLocalVarIndex, varIdx)
 		}
 
 		if instruction.Label == NoLabel {
@@ -28,60 +33,79 @@ func NewProgram(instructions []Instruction, inputs []int) (Program, error) {
 		labels[instruction.Label] = index
 
 	}
-	variables := make([]int, max(maxVar, len(inputs)*2, 1))
 
+	usedInputVars := make([]int, max(len(inputs), maxInputVarIndex+1))
 	for inputIndex, value := range inputs {
-		variables[2*(inputIndex+1)-1] = value
+		usedInputVars[inputIndex] = value
 	}
 
 	return Program{
 		instructions:            instructions,
-		labels:                  labels,
-		variables:               variables,
 		currentInstructionIndex: 0,
+		labels:                  labels,
+		output:                  0,
+		localVariables:          make([]int, maxLocalVarIndex+1),
+		inputs:                  usedInputVars,
+		maxUsedInputVarsIndex:   maxInputVarIndex,
 	}, nil
 }
 
 type Program struct {
+	// instructions of the program
 	instructions []Instruction
-	// labels contains a map from Label to the number of its instruction
-	labels map[Label]int
-	// variables contains a map from each variable to its value
-	variables               []int
+	// currentInstructionIndex is the index of the next instruction to be executed.
+	// if its value is more than len(instructions), this mean that the program is terminated.
 	currentInstructionIndex int
+	// labels contains a map from Label to the number of its instruction
+	// the goal of saving this map is faster execution time.
+	labels map[Label]int
+
+	// output contains the current value of Y
+	output int
+	// localVariables contains the value of the local variables
+	localVariables []int
+	// inputs contains the value of the input variables
+	inputs []int
+	// maxUsedInputVarsIndex is the index (in the above array) of the biggest
+	// (with respect to the order of variables) input variable used in the program
+	maxUsedInputVarsIndex int
 }
 
 func (p *Program) String() string {
-	fmt.Println("Instructions")
+	var output strings.Builder
+	output.WriteString(fmt.Sprintln("Instructions"))
 	for i, instruction := range p.instructions {
-		fmt.Println(i, instruction.String())
+		output.WriteString(fmt.Sprintln(i, instruction.String()))
 	}
-	fmt.Println("Labels")
+	output.WriteString(fmt.Sprintln("Labels"))
 	for label, instructionNumber := range p.labels {
-		fmt.Println(label.String(), "->", instructionNumber)
+		output.WriteString(fmt.Sprintln(label.String(), "->", instructionNumber))
 	}
 
-	fmt.Println("variables")
-	for index, value := range p.variables {
-		fmt.Println(Variable(index+1).String(), "->", value)
+	output.WriteString(fmt.Sprintln("Input variables"))
+	for index, value := range p.inputs {
+		output.WriteString(fmt.Sprintln(Variable((index+1)*2).String(), "->", value))
 	}
-	return ""
+
+	output.WriteString(fmt.Sprintln("Local variables"))
+	for index, value := range p.inputs {
+		output.WriteString(fmt.Sprintln(Variable((index+1)*2+1).String(), "->", value))
+	}
+	return output.String()
 }
 
 func (p *Program) Snapshot() string {
-	var builder, inputs, variables strings.Builder
+	var builder, inputs, localVariables strings.Builder
 	builder.WriteString(fmt.Sprintf("%d ", p.currentInstructionIndex+1))
-	for i, value := range p.variables[1:] {
-		switch i % 2 {
-		case 0:
-			inputs.WriteString(fmt.Sprintf("%d ", value))
-		case 1:
-			variables.WriteString(fmt.Sprintf("%d ", value))
-		}
+	for _, value := range p.inputs[:p.maxUsedInputVarsIndex+1] {
+		inputs.WriteString(fmt.Sprintf("%d ", value))
+	}
+	for _, value := range p.localVariables {
+		localVariables.WriteString(fmt.Sprintf("%d ", value))
 	}
 	builder.WriteString(inputs.String())
-	builder.WriteString(variables.String())
-	builder.WriteString(strconv.Itoa(p.variables[0]))
+	builder.WriteString(localVariables.String())
+	builder.WriteString(strconv.Itoa(p.output))
 	return builder.String()
 }
 
@@ -95,13 +119,13 @@ func (p *Program) Step() (terminated bool) {
 	case InstructionTypeNoOp:
 		p.currentInstructionIndex += 1
 	case InstructionTypeIncr:
-		p.variables[currentInstruction.Var-1] += 1
+		p.changeVariableValue(currentInstruction.Var, 1)
 		p.currentInstructionIndex += 1
 	case InstructionTypeDecr:
-		p.variables[currentInstruction.Var-1] -= 1
+		p.changeVariableValue(currentInstruction.Var, -1)
 		p.currentInstructionIndex += 1
 	default: // IF V != 0 GOTO L instruction
-		value := p.variables[currentInstruction.Var-1]
+		value := p.variableValue(currentInstruction.Var)
 		if value == 0 {
 			p.currentInstructionIndex += 1
 			return p.IsTerminated()
@@ -115,6 +139,36 @@ func (p *Program) Step() (terminated bool) {
 		p.currentInstructionIndex = instructionIndex
 	}
 	return p.IsTerminated()
+}
+
+// variableValue returns the variable value from registry arrays
+// only if it exists, otherwise it returns 0
+func (p *Program) variableValue(v Variable) int {
+	vType, idx := v.TypeAndIndex()
+	switch vType {
+	case VariableTypeOutput:
+		return p.output
+	case VariableTypeLocal:
+		return p.localVariables[idx]
+	case VariableTypeInput:
+		return p.inputs[idx]
+	default: //unsupported value type
+		return 0
+	}
+}
+
+// changeVariableValue changes variable value in registry arrays
+// only if it exists, otherwise it's a NO-OP
+func (p *Program) changeVariableValue(v Variable, diff int) {
+	vType, idx := v.TypeAndIndex()
+	switch vType {
+	case VariableTypeOutput:
+		p.output += diff
+	case VariableTypeLocal:
+		p.localVariables[idx] += diff
+	case VariableTypeInput:
+		p.inputs[idx] += diff
+	}
 }
 
 func (p *Program) IsTerminated() bool {
